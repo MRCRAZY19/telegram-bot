ce9225d1b3285339ef413073e1ed2ac0845c7e0e
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-22 13:53:08 +0100

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,11 +1,11 @@
 global.token = ''
-global.owner_id = '950419831'
-global.owner_user = 'wildan_izzudin'
+عالمي . owner_id  =  '2057515577'
+عالمي . owner_user       =       'MrYOUCHN'
 global.APIs = { 
-	neoxr: 'https://api.neoxr.eu.org/api'
+	neoxr: 'https://api.neoxr.eu.org/ZILhx15V'
 }
 global.APIKeys = { 
-	'https://api.neoxr.eu.org/api': 'yourkey'
+	'https://api.neoxr.eu.org/api': '5552008595:AAGKSnpP1OUWjbMxGujWG9Z53xvSvP0_uuM'
 }
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 
